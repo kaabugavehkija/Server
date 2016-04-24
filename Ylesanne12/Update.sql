757451cba6abbf1ea70014b4aef74db52bd16431
kaabugavehkija <mustasaarmario@gmail.com>
2016-04-24 19:48:55 +0300

    Ylesanne

---
+++ Ylesanne12/Update.sql
@@ -0,0 +1,7 @@
+UPDATE marioloomaaed SET liik="pildid/bear.png" WHERE id=1;
+UPDATE marioloomaaed SET liik="pildid/cat.png" WHERE id=2;
+UPDATE marioloomaaed SET liik="pildid/cow.png" WHERE id=3;
+UPDATE marioloomaaed SET liik="pildid/dog.png" WHERE id=4;
+UPDATE marioloomaaed SET liik="pildid/monkey.png" WHERE id=5;
+UPDATE marioloomaaed SET liik="pildid/pig.png" WHERE id=6;
+UPDATE marioloomaaed SET liik="pildid/rabbit.png" WHERE id=7;
